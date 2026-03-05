ffa1edeb030bd85d39dffc7fc8ce29a87c4187fe
akash <iakash0511@gmail.com>
2026-03-06 02:08:33 +0530

    packing video updated

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -48,9 +48,9 @@ export default async function ProductPage({ params }: { params?: Promise<{ slug:
         <div className="flex flex-col gap-8">
           <ProductInfo product={product} />
           <OrderPackingSection
-            videoMp4Url={"https://res.cloudinary.com/dltpeh88i/video/upload/q_auto:eco,f_mp4,w_720,vc_auto/tinivo/packing/tinivo-packing/"+slug+".mp4"}
-            videoWebmUrl={"https://res.cloudinary.com/dltpeh88i/video/upload/q_auto:eco,f_webm,w_720,vc_auto/tinivo/packing/tinivo-packing/"+slug+".webm"}
-            posterUrl={"https://res.cloudinary.com/dltpeh88i/video/upload/so_0,f_jpg,q_auto,w_720/tinivo/packing/tinivo-packing/"+slug+".jpg"}
+            videoMp4Url={"https://res.cloudinary.com/dltpeh88i/video/upload/v1772742302/default-tinivo-packing-video_ippdzr.mp4"}
+            videoWebmUrl={"https://res.cloudinary.com/dltpeh88i/video/upload/v1772742302/default-tinivo-packing-video_ippdzr.webm"}
+            posterUrl={"https://res.cloudinary.com/dltpeh88i/video/upload/so_0,f_jpg,q_auto,w_720/tinivo/packing/tinivo-packing-default_oeaiv3.jpg"}
           />
           <ProductTabs description={product.description} shipping={product.shipping} care={product.care} />
         </div>
